30ca1fc94acb4b1caaa88e83ff88867adfce3f54
Giovanni <gio.a.98@gmail.com>
2022-08-15 17:34:51 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chaznex.photogallery',
+  appName: 'Photo Gallery',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
