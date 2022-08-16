c7eda91cf234db8b9202d1ae5087e3412765335b
Giovanni <gio.a.98@gmail.com>
2022-08-16 18:34:49 +0200

    Native Camera API - save photos on filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,24 +1,36 @@
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera'
 import { useState } from 'react'
+import { Directory, Filesystem } from '@capacitor/filesystem'
 
 export function usePhotoGallery() {
   const [photos, setPhotos] = useState<UserPhoto[]>([])
+
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     })
+
     const fileName = new Date().getTime() + '.jpeg'
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath
-      },
-      ...photos
-    ]
+    const savedFileImage = await savePicture(photo, fileName)
+    const newPhotos = [savedFileImage, ...photos]
     setPhotos(newPhotos)
   }
+
+  const savePicture =async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!)
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    })
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    }
+  }
+
   return {
     photos,
     takePhoto
@@ -28,4 +40,21 @@ export function usePhotoGallery() {
 export interface UserPhoto {
   filepath: string
   webviewPath?: string
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path)
+  const blob = await response.blob()
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onerror = reject
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result)
+      } else {
+        reject('method did not return a string')
+      }
+    }
+    reader.readAsDataURL(blob)
+  })
 }
